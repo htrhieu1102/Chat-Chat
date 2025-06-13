edb1b40ed957bb2a202dfd0787f74594128368ca
htrhieu1102 <20130260@st.hcmuaf.edt.vn>
2025-06-14 00:45:27 +0700

    change database connection

--- backend/src/main/java/chatchat/entity/Message.java
+++ backend/src/main/java/chatchat/entity/Message.java
@@ -1,13 +1,34 @@
 package chatchat.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    // @OneToMany
+    // private User sender;
+
+    // private String message;
+
+    // private LocalDateTime createAt;
+
+    // private LocalDateTime updateAt;
+ 
 
 }
